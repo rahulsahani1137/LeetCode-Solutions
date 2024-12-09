7f546da691d5ff5193d184e5c367ebd772ca3922
Rahul Sahani <66432121+rahulsahani1137@users.noreply.github.com>
2024-12-09 11:17:29 +0530

    Time: 1 ms (100%), Space: 47.2 MB (40.09%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.java
@@ -5,18 +5,18 @@ class Solution {
         int index = n - 1;
         int leftPointer = 0;
         int rightPointer = n - 1;
-        while(leftPointer <= rightPointer){
+        while (leftPointer <= rightPointer) {
             int squareOfLeft = nums[leftPointer] * nums[leftPointer];
             int squareOfRight = nums[rightPointer] * nums[rightPointer];
-            if(squareOfRight > squareOfLeft){
+            if (squareOfRight > squareOfLeft) {
                 result[index] = squareOfRight;
-                rightPointer --;
+                rightPointer--;
             } else {
-                 result[index] = squareOfLeft;
-                 leftPointer ++;
+                result[index] = squareOfLeft;
+                leftPointer++;
             }
-            index --;
+            index--;
         }
         return result;
-    } 
+    }
 }
